9f52d453769eb1903a0304de9b3cc646378e4ff6
ardell.s.klemme@gmail.com <ardell.s.klemme@gmail.com@2cd2ae4f-6072-d6a0-c631-da1f516d7b3f>
2011-11-17 18:42:47 +0000

    [Issue 8] Made correction to interface class based on work in EnergySince.java. EnergySince is not yet ready to upload to the repository.

--- trunk/src/edu/hawaii/halealohacli/command/Command.java
+++ trunk/src/edu/hawaii/halealohacli/command/Command.java
@@ -1,5 +1,7 @@
 package edu.hawaii.halealohacli.command;
 
+import org.wattdepot.client.WattDepotClientException;
+
 /**
  * @author Ardell Klemme
  * 
@@ -8,20 +10,20 @@ package edu.hawaii.halealohacli.command;
  */
 public interface Command {
   
-  //verfies that the commandName variable matches our current command class
+  //Verifies that the commandName variable matches our current command class
   /**
    * Verifies the correct command name argument matches the class, and the correct number of args.
    * 
-   * @param commandName - name of command to check.
    * @param commandArguments - arguments to verify.
    * @return boolean value to see if command was called correctly.
    */
-  boolean verify (String commandName, String ... commandArguments);
+  boolean verify (String ... commandArguments);
   //runs the command with variable number of parameters
   /**
    * Basic run method that will run the command accepting list of strings as parameters.
    * 
    * @param parameters needed for running command.
+   * @throws WattDepotClientException 
    */
-  public void run (String ... parameters);
+  public void run (String ... parameters) throws WattDepotClientException;
 }
