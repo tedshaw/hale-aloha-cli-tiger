00d6162c4e400de8457ddfce8adfe3be5a3bb841
ardell.s.klemme@gmail.com <ardell.s.klemme@gmail.com@2cd2ae4f-6072-d6a0-c631-da1f516d7b3f>
2011-11-17 20:51:58 +0000

    [Issue 8] Made another revision to Command Interface class, added WattDepotClient argument for commands.

--- src/edu/hawaii/halealohacli/command/Command.java
+++ src/edu/hawaii/halealohacli/command/Command.java
@@ -1,5 +1,6 @@
 package edu.hawaii.halealohacli.command;
 
+import org.wattdepot.client.WattDepotClient;
 import org.wattdepot.client.WattDepotClientException;
 
 /**
@@ -10,20 +11,11 @@ import org.wattdepot.client.WattDepotClientException;
  */
 public interface Command {
   
-  //Verifies that the commandName variable matches our current command class
-  /**
-   * Verifies the correct command name argument matches the class, and the correct number of args.
-   * 
-   * @param commandArguments - arguments to verify.
-   * @return boolean value to see if command was called correctly.
-   */
-  boolean verify (String ... commandArguments);
-  //runs the command with variable number of parameters
   /**
    * Basic run method that will run the command accepting list of strings as parameters.
    * 
    * @param parameters needed for running command.
    * @throws WattDepotClientException 
    */
-  public void run (String ... parameters) throws WattDepotClientException;
+  public void run (WattDepotClient client, String ... parameters) throws WattDepotClientException;
 }
