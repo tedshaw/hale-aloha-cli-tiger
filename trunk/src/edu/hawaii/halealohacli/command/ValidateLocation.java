6fa9e6db6fa9119d6611ccc0e155abdde9c42fcf
ardell.s.klemme@gmail.com <ardell.s.klemme@gmail.com@2cd2ae4f-6072-d6a0-c631-da1f516d7b3f>
2011-11-17 21:51:46 +0000

    [Issue 10] added ValidateLocation class to the repository.  We'll use this in command processor class to check locations.

---
+++ trunk/src/edu/hawaii/halealohacli/command/ValidateLocation.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package edu.hawaii.halealohacli.command;
+
+import java.util.SortedSet;
+
+/**
+ * Verifies whether or not a given location is either a tower or lounge.
+ * 
+ * @author Ardell Klemme
+ *
+ */
+public class ValidateLocation {
+  
+  private static final String[] TOWERS = {"Mokihana", "Ilima", "Lehua", "Lokelani"};
+  private static final String[] ALPHA = {"-A", "-B", "-C", "-D", "-E"};
+  private static SortedSet<String> locations = null;
+  
+  /**
+   * Generates a complete listing of all valid location arguments to check arguments against.
+   * 
+   * @return SortedSet<String> locations - a collection of all possible valid location arguments.
+   */
+  public SortedSet<String> availableLounges() {
+    
+    for (String tower : TOWERS) {
+      
+      for (String location : ALPHA) {
+        locations.add(tower + location);   
+      }
+      
+      locations.add(tower);
+    }
+    return locations;
+  }
+  
+  /**
+   * Checks to see if a string argument is contained in the list of all valid locations.
+   * 
+   * @param argument is the user submitted location [tower | lounge].
+   * @return boolean value of whether or not the location is valid.
+   */
+  public static boolean isValid (String argument) {
+    return locations.contains(argument);
+  }
+}
