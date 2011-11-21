13a4471fc39a58eeb7aebf021aabbc96b51a2597
matt.mizumoto@gmail.com <matt.mizumoto@gmail.com@2cd2ae4f-6072-d6a0-c631-da1f516d7b3f>
2011-11-21 06:33:37 +0000

    [issue 15] [issue 16] Modified Command, CurrentPower, and CommandProcessor Classes

--- trunk/src/edu/hawaii/halealohacli/Main.java
+++ trunk/src/edu/hawaii/halealohacli/Main.java
@@ -2,26 +2,26 @@ package edu.hawaii.halealohacli;
 
 import java.util.Scanner;
 import org.wattdepot.client.WattDepotClient;
-
 import edu.hawaii.halealohacli.processor.CommandProcessor;
 
 /**
  * Main class that prompts the user for commands.
+ * 
  * @author Matthew Mizumoto
- *
+ * 
  */
 public class Main {
-  
+
   /**
    * 
    * @param args no arguments required
    * @throws Exception if cannot make connection to server
    */
-  public static void main (String[] args) throws Exception  {
+  public static void main(String[] args) throws Exception {
     String url = "http://server.wattdepot.org:8190/wattdepot/";
     // Create a client.
     WattDepotClient client = new WattDepotClient(url);
-    // Check to make sure a connection can be made. 
+    // Check to make sure a connection can be made.
     // If no connection, then exit right now.
     if (client.isHealthy()) {
       System.out.println("Sucscessfully connected to the Hale Aloha Wattdepot Server");
@@ -30,14 +30,14 @@ public class Main {
       System.out.format("Could not connect to: %s%n", url);
       return;
     }
-    
+
     Scanner keybd = new Scanner(System.in);
     String input = keybd.nextLine();
-      while (!("quit".equals(input)))  {
-        CommandProcessor command = new CommandProcessor(input);
-        command.run();
-        input = keybd.nextLine();
-      }
+    while (!("quit".equals(input))) {
+      CommandProcessor command = new CommandProcessor(client, input);
+      command.run();
+      input = keybd.nextLine();
+    }
   }
 
 }
--- trunk/src/edu/hawaii/halealohacli/command/Command.java
+++ trunk/src/edu/hawaii/halealohacli/command/Command.java
@@ -1,22 +1,26 @@
 package edu.hawaii.halealohacli.command;
 
-import org.wattdepot.client.WattDepotClient;
-
 /**
  * @author Ardell Klemme
  * 
  * Implements basic interface for Commands.
- *
+ * 
  */
 public interface Command {
-  
+
+  /**
+   * Basic validation method thats tests to see the correct parameters were entered.
+   * @param command the command to be tested
+   * @return true if the command's parameters are correct.
+   */
+  public boolean isValid(String command);
+
   /**
    * Basic run method that will run the command accepting list of strings as parameters.
    * 
-   * @param client is the WattDepotClient passed from the Main class.
-   * @param parameters needed for running command.
-   * @throws Exception this is a catch-all for exceptions.  We'll generate more detailed exceptions
-   * at a later date. 
+   * @param command command needed to run the program
+   * @throws Exception this is a catch-all for exceptions. We'll generate more detailed exceptions
+   * at a later date.
    */
-  public void run (WattDepotClient client, String ... parameters) throws Exception;
+  public void run(String command) throws Exception;
 }
