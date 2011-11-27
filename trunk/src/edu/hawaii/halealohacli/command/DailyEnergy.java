bf7e8952789c92ea0540c01306e6cd4bf3b1f528
tnchida@gmail.com <tnchida@gmail.com@2cd2ae4f-6072-d6a0-c631-da1f516d7b3f>
2011-11-27 19:56:02 +0000

    [issue 24] added run() method in DailyEnergy.java

--- trunk/src/edu/hawaii/halealohacli/command/DailyEnergy.java
+++ trunk/src/edu/hawaii/halealohacli/command/DailyEnergy.java
@@ -8,41 +8,62 @@ import org.wattdepot.client.WattDepotClient;
 
 /**
  * Used for obtaining the amount of energy used on a particular day.
+ * 
  * @author Terrence Chida
- *
+ * 
  */
-public class DailyEnergy {
+public class DailyEnergy implements Command {
 
-  /*
-  private WattDepotClient client;
-  */
+  // client for this command class, to be set upon call to constructor.
+  WattDepotClient client;
 
   /**
-   * Constructor for the DailyEnergy class.
-   * @param client The client passed in from the main method.
+   * Initializes the RankTowers class with a WattDepotClient that is passed from
+   * the Command Processor class after it has been checked for a healthy
+   * connection.
+   * 
+   * @param client
+   *          The client to be used for this command.
+   * @throws Exception
+   *          If there is an error accessing the server or user input errors.
    */
-  /*
-  public DailyEnergy(WattDepotClient client) {
+  public DailyEnergy(WattDepotClient client) throws Exception {
     this.client = client;
   }
-  */
+
+  /**
+   * Verifies whether or not command has the correct number of arguments.
+   * 
+   * @param command
+   *          The string of command arguments.
+   * @return true if correct number of arguments.
+   */
+  @Override
+  public boolean isValid(String command) {
+    String[] splitCommand = command.split(" ");
+    if (splitCommand.length == 3) {
+      return true;
+    }
+    return false;
+  }
 
   /**
    * Runs the program.
    * 
-   * @param args
+   * @param command
    *          Command line arguments.
    * @throws Exception
-   *           Any Exception.
+   *          If there is an error accessing the server or user input errors.
    */
-  // @Override
-  public static void main(String[] args) throws Exception {
-    if (args.length == 2) {
+  @Override
+  public void run(String command) throws Exception {
+    String[] commandAndArgs = command.split(" ");
+
+    if (isValid(command)) {
       List<String> ydm = new ArrayList<String>();
       Double energy;
-      String url = "http://server.wattdepot.org:8190/wattdepot/";
-      String tower = args[0];
-      String date = args[1];
+      String tower = commandAndArgs[1];
+      String date = commandAndArgs[2];
       if (VerifyDate.isValidDate(date)) {
         StringTokenizer st = new StringTokenizer(date, "-");
         while (st.hasMoreTokens()) {
@@ -51,37 +72,30 @@ public class DailyEnergy {
         Integer year = Integer.parseInt(ydm.get(0));
         Integer month = Integer.parseInt(ydm.get(1));
         Integer day = Integer.parseInt(ydm.get(2));
-        // Create a client.
-        WattDepotClient client = new WattDepotClient(url);
-        // Check to make sure a connection can be made.
-        // If no connection, then exit right now.
-        if (client.isHealthy()) {
-          XMLGregorianCalendar now = client.getLatestSensorData(tower)
-              .getTimestamp();
-          int thisMonth = now.getMonth();
-          int today = now.getDay();
-          XMLGregorianCalendar now2 = client.getLatestSensorData(tower)
-              .getTimestamp();
-          XMLGregorianCalendar start = DailyEnergy.setDay(now, year, month, day,
-              0, 0, 0, 0);
-          XMLGregorianCalendar end = DailyEnergy.setDay(now2, year, month, day,
-              23, 59, 59, 999);
-          // Check to see if input date is before today's date.
-          if (end.getMonth() <= thisMonth && end.getDay() < today) {
-            energy = client.getEnergyConsumed(tower, start, end, 0);
-            System.out.println(start);
-            System.out.println(end);
-            System.out.print(tower + "'s energy consumption for " + date
-                + " was: " + (Math.round(energy) / 1000) + " kWh.");
-          }
-          else {
-            System.out.println("Date must be before today.");
-          }
+        XMLGregorianCalendar now = client.getLatestSensorData(tower)
+            .getTimestamp();
+        int thisMonth = now.getMonth();
+        int today = now.getDay();
+        XMLGregorianCalendar now2 = client.getLatestSensorData(tower)
+            .getTimestamp();
+        XMLGregorianCalendar start = DailyEnergy.setDay(now, year, month, day,
+            0, 0, 0, 0);
+        XMLGregorianCalendar end = DailyEnergy.setDay(now2, year, month, day,
+            23, 59, 59, 999);
+        // Check to see if input date is before today's date.
+        if (end.getMonth() <= thisMonth && end.getDay() < today) {
+          energy = client.getEnergyConsumed(tower, start, end, 0);
+          System.out.println(start);
+          System.out.println(end);
+          System.out.print(tower + "'s energy consumption for " + date
+              + " was: " + (Math.round(energy) / 1000) + " kWh.");
         }
         else {
-          System.out.format("Could not connect to: %s%n", url);
-          return;
+          System.out.println("Date must be before today.");
         }
+      } // end verify date
+      else {
+        System.out.println("Sorry, you have entered an invalid date.");
       }
     } // end if one arg provided.
 
@@ -108,8 +122,8 @@ public class DailyEnergy {
    *          The millisecond to set.
    * @return The XMLGregorianCalendar date.
    */
-  public static XMLGregorianCalendar setDay(XMLGregorianCalendar today, int year,
-      int month, int day, int hour, int min, int sec, int millisec) {
+  public static XMLGregorianCalendar setDay(XMLGregorianCalendar today,
+      int year, int month, int day, int hour, int min, int sec, int millisec) {
     XMLGregorianCalendar d = today;
     d.setYear(year);
     d.setMonth(month);
