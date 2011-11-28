58e3f0e56c1c59905979de1bf0bf94f131efece4
ardell.s.klemme@gmail.com <ardell.s.klemme@gmail.com@2cd2ae4f-6072-d6a0-c631-da1f516d7b3f>
2011-11-28 09:49:57 +0000

    [Issue 18] EnergySince test class at least tests two methods now, rather than just the verify.  Will think about how to rework the run to get better junit tests in the future.

--- trunk/src/edu/hawaii/halealohacli/command/EnergySinceTest.java
+++ trunk/src/edu/hawaii/halealohacli/command/EnergySinceTest.java
@@ -3,6 +3,7 @@ package edu.hawaii.halealohacli.command;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.wattdepot.client.WattDepotClient;
 
@@ -17,6 +18,7 @@ public class EnergySinceTest {
   EnergySince energyTest = new EnergySince(testClient);
   
 
+
   /**
    * Tests the validation of the number of arguments for energy-since command.
    */
@@ -25,5 +27,13 @@ public class EnergySinceTest {
     assertTrue("Number of commands is valid", energyTest.isValid("energy-since Lehua 2011-11-01") );
     assertFalse("Number of commands is invalid", energyTest.isValid("energy-since") );
   }
-
+  
+  /**
+   * Test to make sure that the checkTens method creates a string that is of the correct format.
+   */
+  @Test
+  public void testCheckTens() {
+    assertEquals("Make sure 07 shows up as 07 and not 7", "07", energyTest.checkTens(7));
+    assertEquals("Make sure 17 shows up as 17", "17", energyTest.checkTens(17));
+  }
 }
