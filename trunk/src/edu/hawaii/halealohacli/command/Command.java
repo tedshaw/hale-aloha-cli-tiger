468becbfae4a7e43b476f7fac3913e9fa0d24d4a
tnchida@gmail.com <tnchida@gmail.com@2cd2ae4f-6072-d6a0-c631-da1f516d7b3f>
2011-11-29 09:49:25 +0000

    [Issue 34] Made changes to code layout and documentation.

--- trunk/src/edu/hawaii/halealohacli/command/Command.java
+++ trunk/src/edu/hawaii/halealohacli/command/Command.java
@@ -1,26 +1,33 @@
 package edu.hawaii.halealohacli.command;
 
 /**
+ * Implements basic interface for Commands.
+ * 
  * @author Ardell Klemme
  * 
- * Implements basic interface for Commands.
  * 
  */
 public interface Command {
 
   /**
-   * Basic validation method thats tests to see the correct parameters were entered.
-   * @param command the command to be tested
+   * Basic validation method thats tests to see the correct parameters were
+   * entered.
+   * 
+   * @param command
+   *          the command to be tested
    * @return true if the command's parameters are correct.
    */
   public boolean isValid(String command);
 
   /**
-   * Basic run method that will run the command accepting list of strings as parameters.
+   * Basic run method that will run the command accepting list of strings as
+   * parameters.
    * 
-   * @param command command needed to run the program
-   * @throws Exception this is a catch-all for exceptions. We'll generate more detailed exceptions
-   * at a later date.
+   * @param command
+   *          command needed to run the program
+   * @throws Exception
+   *           this is a catch-all for exceptions. We'll generate more detailed
+   *           exceptions at a later date.
    */
   public void run(String command) throws Exception;
 }
