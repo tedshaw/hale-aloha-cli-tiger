95fd44d1c111a5e02c7dd93ea8e2dbf2816e91a0
tnchida@gmail.com <tnchida@gmail.com@2cd2ae4f-6072-d6a0-c631-da1f516d7b3f>
2011-11-29 09:48:20 +0000

    [Issue 36] Added another test case to TestDailyEnergy.java

--- src/edu/hawaii/halealohacli/command/TestDailyEnergy.java
+++ src/edu/hawaii/halealohacli/command/TestDailyEnergy.java
@@ -1,5 +1,6 @@
 package edu.hawaii.halealohacli.command;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -13,14 +14,20 @@ import org.wattdepot.client.WattDepotClient;
 public class TestDailyEnergy {
   WattDepotClient testClient = new WattDepotClient("http://server.wattdepot.org:8190/wattdepot/");
   DailyEnergy energyTest = new DailyEnergy(testClient);
-  String testCommand = "daily-energy Mokihana 2011-11-24";
+  
+  // Test cases which should indicate passes.
+  String pass1 = "daily-energy Mokihana 2011-11-24";
+  
+  // Test cases which should indicate failures.
+  String fail1 = "blah";
 
   /**
    * Test the isValid() method.
    */
   @Test
   public void testIsValid() {
-    assertTrue("Check number of arguments", energyTest.isValid(testCommand));
+    assertTrue("Check number of arguments", energyTest.isValid(pass1));
+    assertFalse("Check number of arguments", energyTest.isValid(fail1));
   } // end testIsValid()
-
+  
 } // end TestDailyEnergy
