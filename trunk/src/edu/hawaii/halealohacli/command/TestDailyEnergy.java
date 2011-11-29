1bf8719749cd0066774dac8fabb83fa2ab41812d
tnchida@gmail.com <tnchida@gmail.com@2cd2ae4f-6072-d6a0-c631-da1f516d7b3f>
2011-11-29 01:16:00 +0000

    [Issue 26] Added TestDailyEnergy.java class

---
+++ trunk/src/edu/hawaii/halealohacli/command/TestDailyEnergy.java
@@ -0,0 +1,26 @@
+package edu.hawaii.halealohacli.command;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.wattdepot.client.WattDepotClient;
+
+/**
+ * Test class for DailyEnergy.java.
+ * @author Terrence Chida
+ *
+ */
+public class TestDailyEnergy {
+  WattDepotClient testClient = new WattDepotClient("http://server.wattdepot.org:8190/wattdepot/");
+  DailyEnergy energyTest = new DailyEnergy(testClient);
+  String testCommand = "daily-energy Mokihana 2011-11-24";
+
+  /**
+   * Test the isValid() method.
+   */
+  @Test
+  public void testIsValid() {
+    assertTrue("Check number of arguments", energyTest.isValid(testCommand));
+  } // end testIsValid()
+
+} // end TestDailyEnergy
