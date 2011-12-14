c32fe1f1fd5eb3d38b236d2e597b3d49f1574f6b
joshuakantonio <joshuakantonio@2cd2ae4f-6072-d6a0-c631-da1f516d7b3f>
2011-12-14 03:57:33 +0000

    [Issue 50] Improved coverage of TestSetBaseline and SetBaseline to above 90%

--- trunk/src/edu/hawaii/halealohacli/command/TestSetBaseline.java
+++ trunk/src/edu/hawaii/halealohacli/command/TestSetBaseline.java
@@ -6,10 +6,10 @@ import org.junit.Test;
 import org.wattdepot.client.WattDepotClient;
 
 /**
- * Junit test class for SetBaseline command. 
- *
+ * Junit test class for SetBaseline command.
+ * 
  * @author Joshua Antonio
- *
+ * 
  */
 public class TestSetBaseline {
   WattDepotClient client = new WattDepotClient("http://server.wattdepot.org:8190/wattdepot/");
@@ -22,6 +22,8 @@ public class TestSetBaseline {
   public void testIsValid() {
     assertTrue("Number of commands is valid", setBaseline.isValid("set-baseline Ilima"));
     assertFalse("Number of commands is invalid", setBaseline.isValid("set-baseline"));
+    assertFalse("Invalid command", setBaseline.isValid("invalid-command args"));
+    assertFalse("Invalid command", setBaseline.isValid("set-baseline Ilima 2011-33-44"));
   }
 
   /**
@@ -31,6 +33,11 @@ public class TestSetBaseline {
   public void testRun() {
     try {
       setBaseline.run("set-baseline Ilima 2011-12-02");
+      setBaseline.run("set-baseline Ilima");
+      setBaseline.run("foo-bar");
+      setBaseline.run("set-baseline foo-bar");
+      setBaseline.run("set-baseline Ilima 2022-22-33");
+      setBaseline.run("set-baseline Ilima 2011-09-25");
     }
     catch (Exception e) {
       // TODO Auto-generated catch block
