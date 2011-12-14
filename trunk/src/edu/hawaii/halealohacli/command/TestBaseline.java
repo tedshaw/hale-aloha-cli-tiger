f7abf3d40733780299888fe11986c95d646723b7
tedshaw@gmail.com <tedshaw@gmail.com@2cd2ae4f-6072-d6a0-c631-da1f516d7b3f>
2011-12-14 04:19:22 +0000

    [Issue 50] Improve testing and test coverage.

---
+++ trunk/src/edu/hawaii/halealohacli/command/TestBaseline.java
@@ -0,0 +1,53 @@
+package edu.hawaii.halealohacli.command;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.wattdepot.client.WattDepotClient;
+
+/**
+ * Junit test class for Baseline class.
+ * 
+ * @author Ted Shaw
+ * 
+ */
+public class TestBaseline {
+  WattDepotClient client = new WattDepotClient("http://server.wattdepot.org:8190/wattdepot/");
+  Baseline defaultBase = new Baseline();
+  Baseline baseline = new Baseline("Ilima");
+
+  /**
+   * Tests getBaseline method.
+   */
+  @Test
+  public void testGetBaseline() {
+    assertEquals(0.0, baseline.getBaseline(1), 0.001);
+    baseline.getBaseline(30);
+  }
+
+  /**
+   * Tests setBaseline method.
+   */
+  @Test
+  public void testSetBaseline() {
+    baseline.setBaseline(1);
+    baseline.setBaseline(1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4);
+  }
+
+  /**
+   * Tests storeToFile method.
+   */
+  @Test
+  public void testStoreToFile() {
+    baseline.storeToFile("Ilima.xml");
+  }
+  
+  /**
+   * Tests retrieveFromFile method.
+   */
+  @Test
+  public void testRetrieveFromFile() {
+    baseline.retrieveFromFile("Ilima.xml");
+    baseline.retrieveFromFile("nofile");
+  }
+  
+}
