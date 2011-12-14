6267f21b14189e7de59974b1009dffbd3d85d54f
ToyLim@gmail.com <ToyLim@gmail.com@2cd2ae4f-6072-d6a0-c631-da1f516d7b3f>
2011-12-14 05:14:35 +0000

    [Issue 36] Added piped, threaded execution to execute the monitor commands

--- src/edu/hawaii/halealohacli/command/TestMonitorGoal.java
+++ src/edu/hawaii/halealohacli/command/TestMonitorGoal.java
@@ -2,11 +2,19 @@ package edu.hawaii.halealohacli.command;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PrintStream;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 import org.wattdepot.client.WattDepotClient;
 
 /**
- * Basic test for MonitorGoal command class.
+ * Basic test for MonitorGoal command class. The multi-threaded test code is from
+ * http://www.codeguru.com/forum/showthread.php?t=453798
  * 
  * @author Ted Shaw
  * 
@@ -22,6 +30,68 @@ public class TestMonitorGoal {
   public void testValid() {
     assertTrue("Number of commands is valid", monitorGoal.isValid("monitor-goal Ilima 85 10"));
     assertFalse("Number of commands is invalid", monitorGoal.isValid("monitor-goal"));
+  }
+
+  /**
+   * Test the run() function.
+   */
+  @Test
+  public void testRun() {
+    PipedOutputStream testOutStream = new PipedOutputStream();
+    InputStream testInStream;
+    try {
+      testInStream = new PipedInputStream(testOutStream);
+    }
+    catch (IOException e) {
+      // e1.printStackTrace();
+      System.err.println("Error encountered while trying to connect input and output pipe.");
+      return;
+    }
+    PrintStream printOut = new PrintStream(testOutStream);
+
+    InputStream old = System.in;
+
+    System.setIn(testInStream);
+    FutureTask<?> theTask = null;
+    // create new task
+    theTask = new FutureTask<Object>(new Runnable() {
+      public void run() {
+        try {
+          monitorGoal.run("monitor-goal Ilima 1 1");
+        }
+        catch (Exception e) {
+          // e.printStackTrace();
+          return;
+        }
+      }
+    }, null);
+
+    // start task in a new thread
+    new Thread(theTask).start();
+
+    // let the task thread execute for 5s, then send the termination keyboard input
+    try {
+      Thread.sleep(5000);
+    }
+    catch (InterruptedException e) {
+      //e.printStackTrace();
+      System.err.println("Interrupt Exception occurred.");
+      return;
+    }
+    printOut.println("Terminate");
+
+    // wait for the execution to finish, timeout after 10 secs
+    try {
+      theTask.get(10L, TimeUnit.SECONDS);
+    }
+    catch (Exception e) {
+      //e.printStackTrace();
+      System.err.println("Timeout Exception occurred.");
+      return;
+    }
+
+    System.setIn(old);
+
   }
 
 }
--- src/edu/hawaii/halealohacli/command/TestMonitorPower.java
+++ src/edu/hawaii/halealohacli/command/TestMonitorPower.java
@@ -2,21 +2,28 @@ package edu.hawaii.halealohacli.command;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PrintStream;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 import org.wattdepot.client.WattDepotClient;
 
 /**
- * Basic test for MonitorPower command class.
+ * Basic test for MonitorPower command class. The multi-threaded test code is from
+ * http://www.codeguru.com/forum/showthread.php?t=453798
  * 
  * @author Ted Shaw
- * 
  */
 public class TestMonitorPower {
   WattDepotClient testClient = new WattDepotClient("http://server.wattdepot.org:8190/wattdepot/");
   MonitorPower monitorPower = new MonitorPower(testClient);
 
   /**
-   * Tests the validation of the number of arguments for rank-towers command.
+   * Tests the validation of the number of arguments for monitor-power command.
    */
   @Test
   public void testValid() {
@@ -24,4 +31,66 @@ public class TestMonitorPower {
     assertFalse("Number of commands is invalid", monitorPower.isValid("monitor-power"));
   }
 
+  /**
+   * Test the run() function.
+   */
+  @Test
+  public void testRun() {
+    PipedOutputStream testOutStream = new PipedOutputStream();
+    InputStream testInStream;
+    try {
+      testInStream = new PipedInputStream(testOutStream);
+    }
+    catch (IOException e) {
+      // e1.printStackTrace();
+      System.err.println("Error encountered while trying to connect input and output pipe.");
+      return;
+    }
+    PrintStream printOut = new PrintStream(testOutStream);
+
+    InputStream old = System.in;
+
+    System.setIn(testInStream);
+    FutureTask<?> theTask = null;
+
+    // create new task
+    theTask = new FutureTask<Object>(new Runnable() {
+      public void run() {
+        try {
+          monitorPower.run("monitor-power Ilima 1");
+        }
+        catch (Exception e) {
+          // e.printStackTrace();
+          return;
+        }
+      }
+    }, null);
+
+    // start task in a new thread
+    new Thread(theTask).start();
+
+    // let the task thread execute for 5s, then send the termination keyboard input
+    try {
+      Thread.sleep(5000);
+    }
+    catch (InterruptedException e) {
+      //e.printStackTrace();
+      System.err.println("Interrupt Exception occurred.");
+      return;
+    }
+    printOut.println("Terminate");
+
+    // wait for the execution to finish, timeout after 10 secs
+    try {
+      theTask.get(10L, TimeUnit.SECONDS);
+    }
+    catch (Exception e) {
+      //e.printStackTrace();
+      System.err.println("Timeout Exception occurred.");
+      return;
+    }
+
+    System.setIn(old);
+
+  }
 }
